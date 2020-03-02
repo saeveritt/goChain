a80b5aaca5f1c5234806829114b1a5e0cce10748
embeddedthought <embeddedthought@gmail.com>
2020-03-02 15:08:16 -0600

    initial commit

---
+++ client/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"golang.org/x/net/context"
+	"flag"
+	"log"
+	"time"
+	"google.golang.org/grpc"
+	"../proto"
+)
+var client proto.BlockchainClient
+func main(){
+	addFlag := flag.Bool("add",false,"add new block")
+	listFlag := flag.Bool("list",false,"get the blockchain")
+	flag.Parse()
+
+	conn, err := grpc.Dial("localhost:8080",grpc.WithInsecure())
+	if err != nil{
+		log.Fatalf("cannot dial server: %v", err)
+	}
+
+	client = proto.NewBlockchainClient(conn)
+
+	if *addFlag{
+		addBlock()
+	}
+	if *listFlag{
+		getBlockchain()
+	}
+}
+
+func addBlock(){
+	block, err := client.AddBlock(context.Background(), &proto.AddBlockRequest{
+		Data: time.Now().String(),
+	})
+	if err != nil{
+		log.Fatalf("unable to add block: %v", err)
+	}
+	log.Printf("new block hash: %s\n", block.Hash)
+}
+
+func getBlockchain(){
+	bc, err := client.GetBlockchain(context.Background(), &proto.GetBlockchainRequest{
+	})
+	if err != nil{
+		log.Fatalf("unable to get blockchain: %v", err)
+	}
+	log.Println("blocks:\n")
+	for _, b := range bc.Blocks{
+		log.Printf("hash: %s, prev block hash: %s, data %s",b.Hash,b.PrevBlockHash,b.Data)
+	}
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net"
+	"./proto"
+	"./server"
+	"google.golang.org/grpc"
+	"golang.org/x/net/context"
+)
+
+
+func main(){
+	listener, err := net.Listen("tcp",":8080")
+	if err != nil{
+		log.Fatalf("unable to listen on 8080 port: %v", err)
+	}
+	srv := grpc.NewServer()
+	proto.RegisterBlockchainServer(srv, &Server{
+		Blockchain: blockchain.NewBlockchain()})
+	srv.Serve(listener)
+}
+
+type Server struct{
+	Blockchain *blockchain.Blockchain
+}
+
+func (s *Server) AddBlock(ctx context.Context,in *proto.AddBlockRequest) (*proto.AddBlockResponse, error){
+	block := s.Blockchain.AddBlock(in.Data)
+
+	return &proto.AddBlockResponse{
+		Hash: block.Hash,
+	},nil
+}
+
+func (s *Server) GetBlockchain(ctx context.Context,in *proto.GetBlockchainRequest) (*proto.GetBlockchainResponse, error){
+	resp := new(proto.GetBlockchainResponse)
+	for _, b := range s.Blockchain.Blocks {
+		resp.Blocks = append(resp.Blocks, &proto.Block{
+			PrevBlockHash: b.PrevBlockHash,
+			Hash: b.Hash,
+			Data: b.Data,
+		})
+	}
+	return resp, nil
+}
+
+
